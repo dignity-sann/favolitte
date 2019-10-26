8e25bf6500fab9172642cf00eca76ccaef84cc55
ne-peer <mileage0110@gmail.com>
2019-10-26 16:19:37 +0900

    update: 不要なサンプルファイルを削除、routerを読み込み

---
+++ src/components/pages/TwitterView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <v-card
+            class="mx-auto"
+            max-width="344"
+        >
+            <v-card-text>
+            <div>Word of the Day</div>
+            <p class="display-1 text--primary">
+                be•nev•o•lent
+            </p>
+            <p>adjective</p>
+            <div class="text--primary">
+                well meaning and kindly.<br>
+                "a benevolent smile"
+            </div>
+            </v-card-text>
+            <v-card-actions>
+            <v-btn
+                text
+                color="deep-purple accent-4"
+            >
+                Learn More
+            </v-btn>
+            </v-card-actions>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            title: "Body",
+            text: "Have a good day!"
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
