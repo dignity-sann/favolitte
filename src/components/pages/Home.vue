1cdef7aeb27c19f03cce2452cbbb0e202a8a961d
ne-peer <mileage0110@gmail.com>
2019-10-26 16:25:30 +0900

    update: add twitter component page link

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,15 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png">
+    <div class="logo-section">
+      <img alt="Vue logo" src="@/assets/logo.png" />
+    </div>
+    <v-card class="mx-auto" max-width="400" tile>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            <router-link to="/twitter">Go to TwitterView</router-link>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item two-line>
+        <v-list-item-content>
+          <v-list-item-title>Two-line item</v-list-item-title>
+          <v-list-item-subtitle>Secondary text</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item three-line>
+        <v-list-item-content>
+          <v-list-item-title>Three-line item</v-list-item-title>
+          <v-list-item-subtitle>Secondary line text Lorem ipsum dolor sit amet,</v-list-item-subtitle>
+          <v-list-item-subtitle>consectetur adipiscing elit.</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home',
+  name: "home",
   components: {}
-}
+};
 </script>
 
 <style scoped>
+.home {
+  width: 100%;
+}
+.logo-section {
+  margin: 0 auto;
+}
 </style>
