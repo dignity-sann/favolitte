a594ca097c9ab88ffe5ab358b297c43e8241dbfd
ne-peer <mileage0110@gmail.com>
2019-10-26 16:32:45 +0900

    update: home

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="home">
-    <div class="logo-section">
-      <img alt="Vue logo" src="@/assets/logo.png" />
-    </div>
-    <v-card class="mx-auto" max-width="400" tile>
+    <v-parallax dark src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+          <h1 class="display-1 font-weight-thin mb-4">Vuetify.js</h1>
+          <h4 class="subheading">Build your application today!</h4>
+        </v-col>
+      </v-row>
+    </v-parallax>
+    <v-card class="mx-auto link-list" max-width="400" tile>
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title>
@@ -41,7 +46,7 @@ export default {
 .home {
   width: 100%;
 }
-.logo-section {
-  margin: 0 auto;
+.link-list {
+  margin-top: 20px;
 }
 </style>
