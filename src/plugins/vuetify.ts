e831743ef414c6328591e6132a6a577cd86064ee
dict <sunday.ktkr.dict@gmail.com>
2019-11-13 23:35:09 +0900

    update アイコン追加

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,3 +1,9 @@
+
+// add Material Design Icons
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
+// add Material Icons
+import 'material-design-icons-iconfont/dist/material-design-icons.css' // Ensure you are using css-loader
+
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 
@@ -5,6 +11,6 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: 'mdi',
+    iconfont: 'md',
   },
 });
