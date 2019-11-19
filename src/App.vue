883e63338a8cced98f01e1e262a3428db0c2dd73
dict <sunday.ktkr.dict@gmail.com>
2019-11-19 23:31:42 +0900

    update メニューのソート、フィルタ追加

--- src/App.vue
+++ src/App.vue
@@ -8,8 +8,8 @@
       <v-list dense>
         <v-subheader>Menu</v-subheader>
         <v-list-item
-          v-for="item in items"
-          :key="item.title"
+          v-for="(item, index) in filtering()"
+          :key="index"
           :to="item.path"
           link
         >
@@ -21,6 +21,9 @@
             <v-list-item-title>
               {{ item.title }}
             </v-list-item-title>
+            <v-list-item-subtitle>
+              {{ item.description }}
+            </v-list-item-subtitle>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -35,8 +38,12 @@
           </v-list-item-action>
 
           <v-list-item-content>
-            <v-list-item-title>Darkmode</v-list-item-title>
-            <v-list-item-subtitle>画面を暗くするよ</v-list-item-subtitle>
+            <v-list-item-title>
+              Darkmode
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              画面を暗くするよ
+            </v-list-item-subtitle>
           </v-list-item-content>
           </v-list-item>
       </v-list>
@@ -90,23 +97,49 @@ export default Vue.extend({
     primaryDrawer: {
       model: null,
     },
-    items: [
+    menus: [
       {
         title: 'Home',
         icon: 'home',
-        path: '/'
+        path: '/',
+        order: 0
       },
       {
-        title: 'twitter見るやつ',
-        icon: 'cake',
-        path: '/twitter'
+        title: 'test',
+        description: 'test',
+        icon: 'work',
+        path: '/twitter',
+        order: 10
       },
       {
-        title: 'About',
+        title: '認証(twitter OAuth)',
         icon: 'info',
-        path: '#'
+        path: '/',
+        order: -1
+      },
+      {
+        title: 'リスト作成',
+        description: '共有できるリストを作成',
+        icon: 'info',
+        path: '/',
+        order: -1
       },
     ],
   }),
+  methods: {
+    filtering: function () {
+      return this.menus.filter((menu) => {
+        return menu.order !== undefined && menu.order !== -1
+      }).sort((a, b) => {
+        if (a.order > b.order) {
+          return 1
+        } else if (a.order < b.order) {
+          return -1
+        } else {
+          return 0
+        }
+      })
+    }
+  },
 });
 </script>
