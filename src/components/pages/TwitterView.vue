6ab6e4b6fa652538d7c099a20ffcfb8f3b46cccf
ne-peer <mileage0110@gmail.com>
2019-11-23 12:24:06 +0900

    update: use axios on twitter view

--- src/components/pages/TwitterView.vue
+++ src/components/pages/TwitterView.vue
@@ -1,41 +1,32 @@
 <template>
-    <div>
-        <v-card
-            class="mx-auto"
-            max-width="344"
-        >
-            <v-card-text>
-            <div>Word of the Day</div>
-            <p class="display-1 text--primary">
-                be•nev•o•lent
-            </p>
-            <p>adjective</p>
-            <div class="text--primary">
-                well meaning and kindly.<br>
-                "a benevolent smile"
-            </div>
-            </v-card-text>
-            <v-card-actions>
-            <v-btn
-                text
-                color="deep-purple accent-4"
-            >
-                Learn More
-            </v-btn>
-            </v-card-actions>
-        </v-card>
-    </div>
+  <div>
+    <v-card class="mx-auto" max-width="344">
+      <v-card-text>
+        <div>Word of the Day</div>
+        <p class="display-1 text--primary">be•nev•o•lent</p>
+        <p>adjective</p>
+        <div class="text--primary">
+          well meaning and kindly.
+          <br />"a benevolent smile"
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text color="deep-purple accent-4">Learn More</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    data(){
-        return {
-            title: "Body",
-            text: "Have a good day!"
-        }
-    }
-}
+  mounted () {
+    axios
+      .get('https://gist.githubusercontent.com/ne-peer/b00023de73a1c4c6789eb06231b60439/raw/mello-yello_member-list.json')
+      .then(response => (console.log(response)))
+  }
+};
 </script>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import vuetify from './plugins/vuetify';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
@@ -15,5 +16,3 @@ new Vue({
   vuetify,
   render: h => h(App)
 }).$mount('#app')
-
-Vue.use(VueAxios, axios)
