a46dbbf0ef52543cf7e9b1db36f73343e215c8bd
ne-peer <mileage0110@gmail.com>
2019-11-23 17:31:50 +0900

    update: いいねリストを取得してカードで表示

--- src/components/pages/TwitterView.vue
+++ src/components/pages/TwitterView.vue
@@ -1,33 +1,56 @@
 <template>
   <div>
-    <v-card class="mx-auto" max-width="344">
-      <v-card-text>
-        <div>Word of the Day</div>
-        <p class="display-1 text--primary">be•nev•o•lent</p>
-        <p>adjective</p>
-        <div class="text--primary">
-          well meaning and kindly.
-          <br />"a benevolent smile"
-        </div>
-      </v-card-text>
+    <v-card v-for="tw of tweets" :key="tw.id" class="mx-auto tw-card" max-width="600">
+      <v-img v-if="tw.entities.media" :src="tw.entities.media[0].media_url" height="800px"></v-img>
+
+      <v-card-title>{{tw.user.name}}</v-card-title>
+
+      <v-card-subtitle>{{tw.text}}</v-card-subtitle>
+
       <v-card-actions>
-        <v-btn text color="deep-purple accent-4">Learn More</v-btn>
+        <v-list-item-avatar color="grey darken-3">
+          <v-img class="elevation-6" :src="tw.user.profile_image_url"></v-img>
+        </v-list-item-avatar>
+
+        <v-btn text>Share</v-btn>
+
+        <v-btn color="purple" text>Explore</v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon @click="show = !show">
+          <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+        </v-btn>
       </v-card-actions>
+
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+
+          <v-card-text>I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.</v-card-text>
+        </div>
+      </v-expand-transition>
     </v-card>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-  mounted () {
-    axios
-      .get('https://gist.githubusercontent.com/ne-peer/b00023de73a1c4c6789eb06231b60439/raw/mello-yello_member-list.json')
-      .then(response => (console.log(response)))
+  data() {
+    return {
+      tweets: []
+    };
+  },
+  mounted() {
+    axios.get("/api/tw/favolites/list").then(res => (this.tweets = res.data));
   }
 };
 </script>
 
 <style scoped>
+.tw-card {
+  margin-top: 4px;
+}
 </style>
