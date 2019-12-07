8eaa89c6d2195d36b8e30c93b94bd610ec690381
tomoya.okabayashi <sunday.ktkr.dict@gmail.com>
2019-12-07 11:28:47 +0900

    update 切り分けした

--- server.js
+++ server.js
@@ -1,29 +1,5 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
-const Twitter = require('twitter')
-
-// Load .env variable
-require('dotenv').config()
-
-const client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_SECRET
-});
-
 app.use(express.static(__dirname + "/dist/"));
-
-app.get('/api/tw/favolites/list', (req, res) => {
-    const params = { screen_name: 'ne_peer' }
-    client.get('favorites/list', params, function (error, tweets, response) {
-        if (!error) {
-            res.status(200).send(tweets)
-        } else {
-            res.status(500).send(error)
-        }
-    });
-});
-
 app.listen(port);
