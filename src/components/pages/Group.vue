4b565023d21ad89d1b5058889c52bc54a7bb3c42
tomoya.okabayashi <sunday.ktkr.dict@gmail.com>
2020-02-23 18:00:57 +0900

    update グループに該当するユーザの取得追加

--- src/components/pages/Group.vue
+++ src/components/pages/Group.vue
@@ -10,7 +10,9 @@
 
     <!-- exists group list on left section -->
     <v-row>
-      <v-col cols="6">
+      <v-col
+        md='6' xs='12'
+      >
         <v-row>
           <v-col cols="6">
             <h3>Group</h3>
@@ -49,64 +51,110 @@
             :error-messages="group.desc.error"
             :counter="group.desc.maxLength"
           ></v-textarea>
+          <v-row>
+            <v-col class="texr-left">
+              <v-btn
+                fab small
+                class="mx-2"
+                color="info"
+                @click="resetGroup"
+              >
+                <v-icon dark>mdi-delete</v-icon>
+              </v-btn>
+            </v-col>
+            <v-col class="text-right">
+              <v-btn
+                fab small
+                class="mx-2"
+                color="error"
+                :disabled="!(group.id.value)"
+                @click="removeGroup"
+              >
+                <v-icon dark>mdi-minus</v-icon>
+              </v-btn>
+              <v-btn
+                fab small
+                class="mx-2"
+                color="primary"
+                @click="updateGroup"
+              >
+                <v-icon dark>mdi-plus</v-icon>
+              </v-btn>
+            </v-col>
+          </v-row>
         </v-form>
+      </v-col>
+
+      <v-col
+        md='6' xs='12'
+      >
         <v-row>
-          <v-col class="text-left">
-            <v-btn
-              class="primary ma-2"
-              @click="resetGroup"
-              :rounded="true"
-            >
-              reset
-            </v-btn>          
+          <!-- add group form on right section -->
+          <v-col cols="12">
+            <h3>
+              <v-icon class="mr-1">list</v-icon>Groups
+            </h3>
+            <div class="mt-4">
+              <v-card class="mx-auto" tile>
+                <v-list-item-group color="primary">
+                  <v-list-item
+                    v-for="group of groups"
+                    :key="group.id"
+                    @click="open(group)"
+                  >
+                    <v-list-item-content>
+                      <v-list-item-title>
+                        {{ group.name }}
+                      </v-list-item-title>
+                      <v-list-item-subtitle>
+                        {{ group.desc }}
+                      </v-list-item-subtitle>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-card>
+            </div>
           </v-col>
-          <v-col class="text-right">
-            <v-btn
-              class="primary ma-2"
-              @click="updateGroup"
-              :rounded="true"
-            >
-              update
-            </v-btn>          
-            <v-btn
-              class="error ma-2"
-              @click="removeGroup"
-              :rounded="true"
-              :disabled="!(group.id.value)"
-            >
-              remove
-            </v-btn>
+          <!-- add group form on right section -->
+          <v-col cols="12">
+            <h3>
+              <v-icon class="mr-1">list</v-icon>Users
+            </h3>
+            <div class="mt-4">
+              <v-card class="mx-auto" tile>
+                <v-list-item-group color="primary">
+                  <v-list-item
+                    v-for="user of group.users"
+                    :key="user.id_str"
+                  >
+                    <v-list-item-avatar>
+                      <v-img
+                        :src="user.profile_image_url"
+                      >
+                      </v-img>
+                    </v-list-item-avatar>
+
+                    <v-list-item-content>
+                      <v-list-item-title v-text="user.name"></v-list-item-title>
+                      <v-list-item-subtitle v-text="'@'.concat(user.screen_name)"></v-list-item-subtitle>
+                    </v-list-item-content>
+
+                    <v-list-item-action>
+                      <v-btn icon>
+                        <v-icon color="error">mdi-delete</v-icon>
+                      </v-btn>
+                    </v-list-item-action>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-card>
+            </div>
           </v-col>
         </v-row>
       </v-col>
-
-      <!-- add group form on right section -->
-      <v-col cols="6">
-        <h3>
-          <v-icon class="mr-1">list</v-icon>Groups
-        </h3>
-        <div class="mt-4">
-          <v-card class="mx-auto" tile>
-            <v-list-item-group color="primary">
-              <v-list-item
-                v-for="group of groups"
-                :key="group.id"
-                @click="open(group)"
-              >
-                <v-list-item-content>
-                  <v-list-item-title>
-                    {{ group.name }}
-                  </v-list-item-title>
-                  <v-list-item-subtitle>
-                    {{ group.desc }}
-                  </v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
-          </v-card>
-        </div>
-      </v-col>
     </v-row>
+    <v-overlay :value="isLoading">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
   </v-container>
 </template>
 
@@ -136,12 +184,18 @@ export default {
           value: '',
           maxLength: 120,
           rules: [
-            v => (v && v.length <= 120) || `Description must be less than 120 characters`
+            v =>  {
+              if (v) {
+                (v.length <= 120) || `Description must be less than 120 characters`
+              }
+            }
           ],
           error: ''
         },
+        users: [],
       },
-      groups: []
+      groups: [],
+      isLoading: false
     };
   },
   async mounted () {
@@ -183,6 +237,9 @@ export default {
         }
         this.$refs.form.reset()
         this.group.id.value = ''
+        this.group.name.value = ''
+        this.group.desc.value = ''
+        this.group.users = []
         this.$emit('showMessage', {
           message: 'Success.',
           color: 'success'
@@ -206,12 +263,51 @@ export default {
     resetGroup: function () {
       this.$refs.form.reset()
       this.group.id.value = ''
+      this.group.name.value = ''
+      this.group.desc.value = ''
+      this.group.users = []
     },
-    open: function(group) {
-      this.group.id.value = group.id
-      this.group.name.value = group.name
-      this.group.desc.value = group.desc
-    }
+    open: async function(group) {
+      this.isLoading = true
+      try {
+        const firestore = firebase.firestore()
+        this.group.id.value = group.id
+        this.group.name.value = group.name
+        this.group.desc.value = group.desc
+        this.group.users = []
+        // get group inner user
+        let twitterIds = []
+        await firestore
+          .collection('users')
+          .where('groups', 'array-contains', group.id)
+          .get()
+          .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+              twitterIds.push(doc.id)
+            })
+          })
+        // get twitter user info 
+        await this.fetchTwitterUserDatas(twitterIds)
+          .then((res) => {
+            this.group.users = res.data;
+          })
+      } finally {
+        this.isLoading = false
+      }
+    },
+    fetchTwitterUserDatas: async function (userIds) {
+      const params = {
+          endpoint: "users/lookup",
+          access_token: this.$store.getters.userTwAcessToken,
+          token_secret: this.$store.getters.userTwTokenSecret,
+          param: {
+            user_id: userIds.join(',')
+          }
+      }
+      return axios.get(`${process.env.VUE_APP_API_BASE_URL}/twitter/api/call`, {
+        params: params
+      })
+    },
   },
 };
 </script>
